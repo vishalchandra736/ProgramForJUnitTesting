8240887d135de13cdbee1a7adb2fb4e0a99d5205
vishalchandra736 <vishalchandra736@gmail.com>
2022-12-27 16:22:24 +0530

    Added : Calculate the Monthly Payment

---
+++ MonthlyPayment.java
@@ -0,0 +1,26 @@
+package com.blz.programforjunittesting;
+
+import java.util.Scanner;
+
+public class MonthlyPayment {
+    public static double monthlyPayment(double P, double R, double Y){
+        double payment, r, n;
+        n = 12 * Y;
+        r = R / ( 12 *100);
+        payment = (P * r) / (1 - Math.pow((1 + r), (-n)));
+        return payment;
+    }
+
+    public static void main(String[] args) {
+        double P, R, Y;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the Principal Amount : ");
+        P = scan.nextInt();
+        System.out.println("Enter the Monthly Interest Rate : ");
+        R = scan.nextInt();
+        System.out.println("Enter the Year to pay off : ");
+        Y = scan.nextInt();
+        System.out.println("Monthly Payment is : " + monthlyPayment(P, R, Y));
+
+    }
+}
